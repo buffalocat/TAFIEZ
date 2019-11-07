a66da1096a654ed1db255e3a12ae3d1497c516ec
chris-hunt-98 <30504169+chris-hunt-98@users.noreply.github.com>
2019-11-08 00:37:28 +0900

    Fixed timing issues
    Now the system will fix itself if it ends up VERY out of sync (which happens frequently when debugging, but shouldn't happen too much during organic play anyway).

--- Sokoban/src/fpstimer.cpp
+++ Sokoban/src/fpstimer.cpp
@@ -15,6 +15,12 @@ void FPSTimer::wait_for_frame_end() {
 	auto sleep_time = nspf_ - frame_time - std::chrono::duration<double, std::nano>(offset_);
 	std::this_thread::sleep_for(sleep_time);
 	frame_start_ += nspf_;
-	auto oversleep = std::chrono::high_resolution_clock::now() - frame_start_;
-	offset_ = 0.9 * offset_ + 0.1 * std::chrono::duration_cast<ns>(oversleep).count();
+	auto cur_time = std::chrono::high_resolution_clock::now();
+	auto oversleep = cur_time - frame_start_;
+	// If we slept a frame later than we should have, something bad happened; reset
+	if (oversleep > nspf_) {
+		oversleep = nspf_;
+		frame_start_ = cur_time - nspf_;
+	}
+	offset_ = 0.8 * offset_ + 0.2 * std::chrono::duration_cast<ns>(oversleep).count();
 }
